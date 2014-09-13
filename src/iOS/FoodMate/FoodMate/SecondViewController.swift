5be0a4d769a6742f2c794002b273df479c8f6774
Jordan Horwich <jordan@seas.2429.apng.wireless-pennnet.upenn.edu>
2014-09-13 02:12:57 -0400

    Initial UI commit

---
+++ src/iOS/FoodMate/FoodMate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FoodMate
+//
+//  Created by Jordan Horwich on 9/13/14.
+//  Copyright (c) 2014 FoodMate. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
